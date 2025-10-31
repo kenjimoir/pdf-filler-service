5581ed69e0a910f8c0ccf350815065168cfab4d0
kenjimoir <kenji.moir@blued.jp>
2025-10-31 19:27:04 +0900

    Update RefreshAppearances.java

--- RefreshAppearances.java
+++ RefreshAppearances.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.util.List;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.form.*;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
 
 public class RefreshAppearances {
   public static void main(String[] args) throws Exception {
@@ -12,7 +14,9 @@ public class RefreshAppearances {
     File in = new File(args[0]);
     File out = new File(args[1]);
     try (PDDocument doc = PDDocument.load(in)) {
-      PDAcroForm form = doc.getDocumentCatalog().getAcroForm();
+      // Bypass PDFBox default fixup (which triggers full refreshAppearances)
+      COSDictionary acroDict = (COSDictionary) doc.getDocumentCatalog().getCOSObject().getDictionaryObject(COSName.ACRO_FORM);
+      PDAcroForm form = acroDict != null ? new PDAcroForm(doc, acroDict) : null;
       if (form != null) {
         // Only rebuild appearances for buttons (checkbox/radio) to avoid font encoding issues
         refreshButtonAppearances(form);
